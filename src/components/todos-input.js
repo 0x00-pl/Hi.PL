b9f32b891b069e2be805450af87106606a747592
pl <0x00.pl@gmail.com>
2016-05-08 00:43:48 +0800

    add recent task

--- src/components/todos-input.js
+++ src/components/todos-input.js
@@ -4,23 +4,40 @@ import {} from 'bootstrap/dist/css/bootstrap.css'
 export default class TodosInput extends Component{
     constructor(){
         super()
-        this.state = {value:''}
+        this.state = {value:'', recent_task:[]}
+    }
+    getRecentMatchs(prefix){
+        if(prefix.length < 3 ){
+            return []
+        }
+        let matched = this.state.recent_task.filter((i) => i.startsWith(prefix))
+        return matched
+    }
+    getRecentMatch(prefix){
+        let matched = getRecentMatchs(prefix)
+        let len = matched.length
+        if(len == 0){
+            return undefined
+        }else{
+            return matched.slice(len-1, len)
+        }
     }
     changeHandle(evt) {
         this.setState({ value: evt.target.value })
     }
     commit(){
+        let recent_task = this.state.recent_task.concat(this.state.value)
         this.props.commitHandle(this.state.value)
-        this.setState({value:""})
+        this.setState({value:"", recent_task:recent_task})
     }
     keydownHandle(evt) {
         if(evt.keyCode === 13){
             this.commit()
+        }else if(evt.keyCode == 9){
+            this.setState({value: this.getRecentMatch(this.state.value) || this.state.value})
         }
     }
     onSelectChange(evt){
-        console.log(evt.target)
-        console.log(evt.target.checked)
         this.props.onSelectChangeHandle(evt.target.checked)
     }
     render(){
@@ -42,7 +59,7 @@ export default class TodosInput extends Component{
                         </button>
                     </span>
                     
-                    <div>{this.state.value}</div>
+                    <div>recent: {(this.getRecentMatchs(this.state.value)).join(', ')}</div>
                 </div>
         )
     }
